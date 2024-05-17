c33c23e61537c31633f70fb1eb6d101081fe8bd3
Sakatoku <akr.sakatoku@gmail.com>
2024-05-18 03:02:20 +0900

    Update image generator to support Replicate.

--- st_generate_image.py
+++ st_generate_image.py
@@ -1,6 +1,9 @@
 # generate_image.py
 # 画像を生成してダウンロードするサンプルプログラム
 
+# Replicate library
+import replicate
+
 # OpenAI library
 from openai import OpenAI
 
@@ -11,7 +14,8 @@ import streamlit as st
 import requests
 import os
 
-# OpenAI APIキーを環境変数に設定する
+# ReplicateとOpenAIのAPIキーを環境変数に設定する
+os.environ["REPLICATE_API_TOKEN"] = st.secrets["Replicate"]["apikey"]
 os.environ["OPENAI_API_KEY"] = st.secrets["OpenAI"]["apikey"]
 
 # Initialize Streamlit
@@ -24,9 +28,24 @@ def generate_prompt(situations):
     return f"""
     Create a photorealistic image of the following situations: {situations}.
     """
+# Replicate APIを呼び出して画像を生成する
+def generate_image_replicate(user_prompt):
+    input = {
+        "prompt": user_prompt
+    }
 
-# 画像を生成する
-def generate_image(client, user_prompt):
+    client = replicate.Client(api_token=os.environ["REPLICATE_API_TOKEN"])
+    output = client.run(
+        "bytedance/sdxl-lightning-4step:727e49a643e999d602a896c774a0658ffefea21465756a6ce24b7ea4165eba6a",
+        input=input
+    )
+    return output[0]
+
+# OpenAI APIを呼び出して画像を生成する
+def generate_image_openai(user_prompt):
+    # OpenAIクライアントを初期化する
+    client = OpenAI()
+
     # プロンプトを生成する
     prompt = generate_prompt(user_prompt)
 
@@ -43,39 +62,24 @@ def generate_image(client, user_prompt):
     image_url = response.data[0].url
     return image_url
 
-# # アクティビティを列挙したdictの各要素について、画像を生成してダウンロードする
-# for time_of_day, activity in activities.items():
-#     # プロンプトを生成する
-#     prompt = generate_prompt(time_of_day, activity)
-
-#     # OpenAI APIを呼び出して画像を生成する
-#     response = client.images.generate(
-#         model="dall-e-3",
-#         prompt=prompt,
-#         size="1024x1024",
-#         quality="standard",
-#         n=1,
-#     )
-
-#     # 画像のURLを返り値から取得する
-#     image_url = response.data[0].url
-
-#     # ファイル名を生成する。7:00 -> photo_0700.jpg
-#     filename = "photo_" + time_of_day.replace(":", "") + ".jpg"
-
-#     # 画像をダウンロードする
-#     image = requests.get(image_url)
-#     with open(f'temp/{filename}', 'wb') as f:
-#         f.write(image.content)
-#         print(f"Downloaded {filename}")
+# 画像を生成する
+def generate_image(platform, user_prompt):
+    if platform == "Replicate":
+        return generate_image_replicate(user_prompt)
+    else:
+        return generate_image_openai(user_prompt)
 
 # Main function
 def main():
     # Initialize Streamlit
     init()
 
-    # OpenAIクライアントを初期化する
-    client = OpenAI()
+    # プラットフォームを選択する
+    platforms = [
+        "Replicate",
+        "OpenAI"
+    ]
+    selected_platform = st.sidebar.selectbox("Platform", platforms, index=0)
 
     # ユーザプロンプトを取得する。ユーザプロンプトが空の場合はここで終了
     user_prompt = st.chat_input("Your prompt")
@@ -86,7 +90,7 @@ def main():
     st.write(f"Prompt: {user_prompt}")
 
     # Generate image
-    image_url = generate_image(client, user_prompt)
+    image_url = generate_image(selected_platform, user_prompt)
 
     # Display the image
     image = requests.get(image_url)
