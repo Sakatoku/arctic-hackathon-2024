e548f5cc9651c04b91ac3fbae34d3036b9b5b24c
THiyama <robot_toru@outlook.jp>
2024-05-20 22:23:02 +0900

    Add error handling if sum_crime column doesn't exist.

--- demo/services/inquiry_plan.py
+++ demo/services/inquiry_plan.py
@@ -92,8 +92,13 @@ def extract_record(session: Session, table_name: str, category_col_name: str, sp
     df_base = session.table(table_name)
     df_base = df_base.with_column("customer_pref_v", call_udf("snowflake.cortex.EMBED_TEXT_768", lit('snowflake-arctic-embed-m'), lit(request_description)))
     df_base = df_base.with_column("cos_sim", call_udf("VECTOR_COSINE_SIMILARITY", col("customer_pref_v"), col("embeded_web_summary")))
-    df_base = df_base.with_column("score", col("cos_sim")+(1-(col("sum_crime")-1291)/(68930-1291))*0.05)
-    df_base.write.mode("overwrite").save_as_table("tourism.public.temp_table", table_type="temporary")
+    try:
+        df_base = df_base.with_column("score", col("cos_sim")+(1-(col("sum_crime")-1291)/(68930-1291))*0.05)
+        df_base.write.mode("overwrite").save_as_table("tourism.public.temp_table", table_type="temporary")
+    except:
+        df_base = df_base.with_column("score", col("cos_sim"))
+        df_base.write.mode("overwrite").save_as_table("tourism.public.temp_table", table_type="temporary")
+    
     for spot_k, spot_v in spots.items():
         df = session.table("tourism.public.temp_table")
         df = df.where(col(category_col_name)==spot_v)
